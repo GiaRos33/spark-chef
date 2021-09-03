4f5d3a15dc7288cbaeaed860d320b483aff84207
GiaRos33 <89648154+GiaRos33@users.noreply.github.com>
2021-09-03 14:24:16 +0200

    Update install.rb

--- recipes/install.rb
+++ recipes/install.rb
@@ -12,7 +12,6 @@ include_recipe "java"
 group node['hops']['group'] do
   gid node['hops']['group_id']
   action :create
-  gid node['hadoop_spark']['gid']
   not_if "getent group #{node['hops']['group']}"
   not_if { node['install']['external_users'].casecmp("true") == 0 }
 end
